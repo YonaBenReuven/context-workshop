d5b591d6c250c916bfa735e0af6c178eac1b90ae
Yona Ben-Reuven <yona@carmel6000.amitnet.org>
2020-02-23 14:23:16 +0200

    added provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import ClassOne from './ClassOne';
 import ClassTwo from './ClassTwo';
@@ -6,38 +6,12 @@ import Func from './Func';
 
 import './App.css';
 
-// import createContext;
-// asign it to a constant and export;
-// implement Context.Provider and asign a value;
-
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            theme: "dark",
-            language: "English"
-        }
-    }
-
-    toggleTheme = () => {
-        const theme = this.state.theme === "dark" ? "light" : "dark";
-        this.setState({ theme });
-    }
-
-    toggleTLanguage = () => {
-        const language = this.state.language === "English" ? "Hebrew" : "English";
-        this.setState({ language });
-    }
-
-    render() {
-        return (
-            <div className={`App ${this.state.theme}-theme-bg`}>
-                <ClassOne />
-                <ClassTwo />
-                <Func />
-            </div>
-        );
-    }
-}
+const App = () => (
+    <div className="App">
+        <ClassOne />
+        <ClassTwo />
+        <Func />
+    </div>
+);
 
 export default App;
---
+++ src/AppProvider.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+// import createContext;
+// asign it to a constant and export;
+// implement Context.Provider and asign a value;
+// wrap the provider around this.props.children;
+
+class AppProvider extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            theme: "dark",
+            language: "English"
+        }
+    }
+
+    toggleTheme = () => {
+        const theme = this.state.theme === "dark" ? "light" : "dark";
+        this.setState({ theme });
+    }
+
+    toggleTLanguage = () => {
+        const language = this.state.language === "English" ? "Hebrew" : "English";
+        this.setState({ language });
+    }
+
+    render() {
+        return null;
+    }
+}
+
+export default AppProvider;
